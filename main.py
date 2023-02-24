ebfe0ba6bffe54f72e83830911ff9ef40d6421cb
ngntrgduc <trungducnguyen1511@gmail.com>
2023-02-24 09:50:37 +0700

    Add cong tac khao thi

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from bs4 import BeautifulSoup as bs
 from pathlib import Path
 import requests
 import re
-import datetime
 
 page = requests.get("https://www.hcmus.edu.vn/sinh-vien")
 soup = bs(page.content, features="lxml")
@@ -14,7 +13,11 @@ links = [''.join(url) for url in news_link]
 raw_dates = [i.text for i in soup.find_all(class_='mod-articles-category-date')]
 dates = [''.join(re.findall('([\d-])', date)) for date in raw_dates]
 
-thong_bao = ['Các thông báo về Đào Tạo', 'Các thông báo về Công tác sinh viên', 'Thông báo khác']
+ctkt = [i for i in soup.find_all(class_='feed-link')]
+thong_bao_ctkt = [''.join(re.sub(r'(\t|\n)', '', news.text)) for news in ctkt]
+ctkt_link = [''.join(re.findall('http.*" ', str(news))) for news in ctkt]
+
+thong_bao = ['Các thông báo về Đào Tạo', 'Các thông báo về Công tác sinh viên', 'Thông báo khác', 'Các thông báo về Khảo thí']
 index = count = 0
 file = Path('README.md').rename(Path('README.md').with_suffix('.txt'))
 with open(file, 'w', encoding='utf-8') as f:
@@ -26,6 +29,10 @@ with open(file, 'w', encoding='utf-8') as f:
         if count == 15:
             count = 0
         f.write(f' - {dates[i]}: [{news[i]}](https://www.hcmus.edu.vn{links[i]})\n')
+    
+    f.write(f'### {thong_bao[index]}\n')
+    for i in range(len(thong_bao_ctkt)):
+        f.writelines(f' - [{thong_bao_ctkt[i]}]({ctkt_link[i][:-2]})\n') # [:-2] to remove unnecessary characters (" )
 f.close()
 
 readme = Path('README.txt')
