bd1dd27b394c711026c6c56cf4fe440c103a8c60
ngntrgduc <trungducnguyen1511@gmail.com>
2024-07-08 15:48:34 +0700

    Refactor code, clutter free, add user-agent when crawling

--- main.py
+++ main.py
@@ -1,49 +1,44 @@
 from bs4 import BeautifulSoup as bs
 import requests
+import os
+from dotenv import load_dotenv
 
-page = requests.get('https://hcmus.edu.vn/tin-tuc/')
-soup = bs(page.content, features='lxml')
-
-news = [i for i in soup.find_all(class_='cmsmasters_post_title entry-title')]
-dates = [i.text for i in soup.find_all(class_='published')]
+load_dotenv()
+USER_AGENT = os.getenv('USER_AGENT')
 
 def remove_year(date):
-    return '/'.join(date.split('/')[:2])
+    return date.rsplit('/', 1)[0]
+
+limit = 5  # Chỉ lấy 5 tin tức mới nhất
+
+page = requests.get('https://hcmus.edu.vn/tin-tuc/', headers={'User-Agent': USER_AGENT})
+soup = bs(page.content, features='lxml')
+news = [_ for _ in soup.find_all(class_='cmsmasters_post_title entry-title')]
+dates = [_.text for _ in soup.find_all(class_='published')]
 
 with open('README.md', 'w', encoding='utf-8') as f:
     f.write(f'### Tin tức\n')
-    for i, (date, new) in enumerate(zip(dates, news)):
-        # Chỉ lấy 5 tin tức mới nhất
-        if i < 5:
-            link = new.find('a')['href']
-            f.write(f" - **{remove_year(date)}**: [{new.text}]({link})\n")
-
+    for date, new in list(zip(dates, news))[:limit]:        
+        link = new.find('a')['href']
+        f.write(f" - **{remove_year(date)}**: [{new.text}]({link})\n")
 
 # Khảo thí và đảm bảo chất lượng
-page = requests.get('http://ktdbcl.hcmus.edu.vn')
+page = requests.get('http://ktdbcl.hcmus.edu.vn', headers={'User-Agent': USER_AGENT})
 soup = bs(page.content, features='lxml')
-
-news = [i for i in soup.find_all(class_='mod-articles-category-title')]
-dates = [i.text for i in soup.find_all(class_='mod-articles-category-date')]
-thong_bao = ['Thông báo', 'Lịch thi', 'Kết quả thi', 'Kết quả phúc khảo']
-index = 0
-
-def in_range(i, n=5):
-    # Chỉ lấy 5 tin tức mới nhất của mỗi mục
-    return (i < n) or \
-           (i >= 15 and i < 15 + n) or \
-           (i >= 35 and i < 35 + n) or \
-           (i >= 55 and i < 55 + n)
+modules = [_ for _ in soup.find_all(class_='category-module')]
+categories = ['Thông báo', 'Lịch thi', 'Kết quả thi', 'Kết quả phúc khảo']
 
 with open('README.md', 'a', encoding='utf-8') as f:
     f.write(f'\n### Thông báo Phòng Khảo thí và Đảm bảo Chất lượng\n')
-    for i, (date, new) in enumerate(zip(dates, news)):
-        # Thông báo sẽ có 15 mục, còn lại sẽ là 20 mục
-        if i == 0 or (i + 5) % 20 == 0:
-            f.write(f'\n#### {thong_bao[index]}\n')
-            index += 1 
+    for i, cat in enumerate(modules):
+        f.write(f'\n#### {categories[i]}\n')
+        for li in cat.find_all('li')[:limit]:
+            a_tag = li.find('a', {'class': 'mod-articles-category-title'})
+            title = a_tag.text.strip()
+            href = a_tag['href'].strip()
 
-        if in_range(i):
-            date = remove_year(date.replace('\t', '').replace('\n', '')[1:-1])
-            link = f"http://ktdbcl.hcmus.edu.vn{new['href']}"
-            f.write(f" - **{date}**: [{new.text.strip()}]({link})\n")
+            raw_date = li.find(class_='mod-articles-category-date').text
+            date = remove_year(raw_date.replace('\t', '').replace('\n', '')[1:-1])
+
+            link = f'http://ktdbcl.hcmus.edu.vn{href}'
+            f.write(f' - **{date}**: [{title}]({link})\n')
